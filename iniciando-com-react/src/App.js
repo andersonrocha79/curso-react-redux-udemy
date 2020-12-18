02fce83f4c65288d79e24fa54e28355c71c7ac30
Anderson Rocha <anderson_rocha79@hotmail.com>
2020-12-18 14:45:16 -0300

    introdução ao react

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+
+    state = 
+    {
+        nome : ''
+    }
+
+    modificarNome = (event) =>
+    {
+
+        let nomeInformado = event.target.value;
+        this.setState({nome: nomeInformado});
+
+    }
+
+    criaComboBox = () =>
+    {
+
+        const opcoes = ["Anderson", "Hiriane", "Philipe", "Nicolle", "Sabrina"];
+        
+        const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>); 
+
+        return (
+            <select>
+                {comboBoxOpcoes}
+            </select>
+        )
+    }
+
+    componentDidMount()
+    {
+        console.log("executou - componentDidMount");
+    }
+
+    render()
+    {
+
+        console.log("executou - render");
+
+        const MeuComboBox = () => this.criaComboBox();
+
+        return (
+
+            <div>
+
+                <input className="text-centralizado" type="text" value={this.state.nome} onChange={this.modificarNome} />
+                <h1>1-Props: Hello {this.props.nome} sua idade é: {this.props.idade} e sua cidade é: {this.props.cidade}</h1>
+                <h1>2-State: Hello {this.state.nome}</h1>
+                <h2>Exemplo ComboBox 1</h2>
+                { this.criaComboBox() }
+                <h2>Exemplo ComboBox 2</h2>
+                <MeuComboBox />
+
+            </div>
+            
+        )
+    }
+
 }
 
 export default App;
